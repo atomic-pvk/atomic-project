5ecaab8e13ed6a1b8b8d8d819580d65767640ebd
AlbinWWoxnerud <albin.woxnerud@gmail.com>
2024-02-06 21:23:13 +0000

    Compressed the FreeRTOS_Demo and updated the autoRunDemo.sh script
    The FreeRTOS_Demo was compressed to remove unnecessary fluff, reducing its size. Additionally, the autoRunDemo.sh script was updated to account for this change.
    
    Modified files:
    - docker/FreeRTOS_Demo.zip
    - docker/autoRunDemo.sh

--- docker/autoRunDemo.sh
+++ docker/autoRunDemo.sh
@@ -6,7 +6,7 @@ if [ ! -d "$folder" ]; then
   unzip FreeRTOS_Demo.zip
 fi
 
-cd FreeRTOS_Demo/Demo/CORTEX_MPS2_QEMU_IAR_GCC/build/gcc
+cd FreeRTOS_Demo/Demo/gcc
 make clean
 make -j
 qemu-system-arm -machine mps2-an385 -cpu cortex-m3 -kernel ./output/RTOSDemo.out -monitor none -nographic -serial stdio
