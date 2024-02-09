d20ca0efa54237105f21ae0fd87afb821a892f78
Vilhelm Prytz <vilhelm@prytznet.se>
2024-02-09 15:10:38 +0100

    make ash script prettier

--- autoRunDemo.sh
+++ autoRunDemo.sh
@@ -1,8 +1,18 @@
-#!bin/ash
+#!/bin/ash
 
 set -e
 
-cd FreeRTOS_Demo/Demo/gcc
-make clean
-make -j
-qemu-system-arm -machine mps2-an385 -cpu cortex-m3 -kernel ./output/RTOSDemo.out -monitor none -nographic -serial stdio
+RED="\e[31m"
+GREEN="\e[32m"
+BOLDGREEN="\e[1;${GREEN}"
+ENDCOLOR="\e[0m"
+
+run() {
+    echo -e "${BOLDGREEN}running cmd ${ENDCOLOR}$1"
+    eval "$1"
+}
+
+run "cd FreeRTOS_Demo/Demo/gcc"
+run "make clean"
+run "make -j"
+run "qemu-system-arm -machine mps2-an385 -cpu cortex-m3 -kernel ./output/RTOSDemo.out -monitor none -nographic -serial stdio"
