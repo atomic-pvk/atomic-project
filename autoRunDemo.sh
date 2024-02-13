de2244ae6e7f29b452ae92a843e86c03f5e2e59f
Vilhelm Prytz <vilhelm@prytznet.se>
2024-02-13 15:40:26 +0100

    some work

--- autoRunDemo.sh
+++ autoRunDemo.sh
@@ -12,7 +12,6 @@ run() {
     eval "$1"
 }
 
-run "cd FreeRTOS_Demo/Demo/gcc"
 run "make clean"
 run "make -j"
 run "qemu-system-arm -machine mps2-an385 -cpu cortex-m3 -kernel ./output/RTOSDemo.out -monitor none -nographic -serial stdio"
