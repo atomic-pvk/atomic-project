1ac80d82b0fec5a47adb20219c56eb798fba5096
Vilhelm Prytz <vilhelm@prytznet.se>
2024-02-15 11:14:08 +0100

    working demo of TCP

--- autoRunDemo.sh
+++ autoRunDemo.sh
@@ -12,6 +12,17 @@ run() {
     eval "$1"
 }
 
+run "cd src"
 run "make clean"
 run "make -j"
-run "qemu-system-arm -machine mps2-an385 -cpu cortex-m3 -kernel ./output/RTOSDemo.out -monitor none -nographic -serial stdio"
+
+QEMU_MAC_ADDRESS="52:54:00:12:34:AD"
+CMD="qemu-system-arm -machine mps2-an385 -cpu cortex-m3 \
+          -kernel ./build/freertos_tcp_mps2_demo.axf \
+          -netdev tap,id=mynet0,ifname=virbr0-nic,script=no \
+          -net nic,macaddr=$QEMU_MAC_ADDRESS,model=lan9118,netdev=mynet0 \
+          -object filter-dump,id=tap_dump,netdev=mynet0,file=/tmp/qemu_tap_dump\
+          -display gtk -m 16M  -nographic -serial stdio \
+          -monitor null -semihosting -semihosting-config enable=on,target=native"
+
+run "$CMD"
