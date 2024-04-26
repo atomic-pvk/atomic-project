97da632fa7ac1409cf4dc2ef46c8bdc562da9386
Vilhelm Prytz <vilhelm@prytznet.se>
2024-04-26 14:07:30 +0200

    print hostname alongside IP

--- src/NTPTask.c
+++ src/NTPTask.c
@@ -120,7 +120,8 @@ static void vNTPTaskSendUsingStandardInterface(void *pvParameters)
             NTP1_server_IP = NTP_server_IPs[i];
             xDestinationAddress.sin_address.ulIP_IPv4 = NTP1_server_IP;
 
-            FreeRTOS_printf(("\n\n Getting from IP: %lu.%lu.%lu.%lu \n\n",
+            FreeRTOS_printf(("\n\n Getting from IP (which is %s): %lu.%lu.%lu.%lu \n\n",
+                pcHostNames[i],
                 NTP1_server_IP & 0xFF,  // Extract the fourth byte
                 (NTP1_server_IP >> 8) & 0xFF,   // Extract the third byte
                 (NTP1_server_IP >> 16) & 0xFF,  // Extract the second byte
