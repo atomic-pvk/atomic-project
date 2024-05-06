a8d3e6424c52d816f70fd7b232207f0f09da5263
Vilhelm Prytz <vilhelm@prytznet.se>
2024-05-06 17:34:26 +0200

    print all files that needs reformatting

--- check-format.sh
+++ check-format.sh
@@ -3,13 +3,23 @@
 # Start searching from the root of the project directory
 start_dir="."
 
+# Initialize an empty string to hold files that require formatting
+files_to_format=""
+
 # Find all C++ and C header and source files, check if they need formatting
 find $start_dir -type f \( -iname "*.cpp" -or -iname "*.hpp" -or -iname "*.c" -or -iname "*.h" \) -exec sh -c '
     for file do
-        clang-format -style=file -output-replacements-xml "$file" | grep "<replacement " >/dev/null
-        if [ $? -eq 0 ]; then
-            echo "File requires formatting: $file"
-            exit 1
+        # Check each file for needed formatting replacements
+        if clang-format -style=file -output-replacements-xml "$file" | grep -q "<replacement "; then
+            # Append the file path to the list
+            files_to_format="$files_to_format$file\n"
         fi
     done
-' sh {} +
+    # If files require formatting, print them and exit with error
+    if [ -n "$files_to_format" ]; then
+        echo -e "These files require formatting:\n$files_to_format"
+        exit 1
+    else
+        echo "All files are properly formatted."
+    fi
+' sh {} + 
